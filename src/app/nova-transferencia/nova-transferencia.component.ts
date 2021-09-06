d2f45b7950214ab2241be02e300edb32b6f38d13
Alefros <alef.r.oliveira@gmail.com>
2021-09-06 01:21:06 -0300

    Aplicação de estilos ao componente

--- src/app/nova-transferencia/nova-transferencia.component.ts
+++ src/app/nova-transferencia/nova-transferencia.component.ts
@@ -3,7 +3,7 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-nova-transferencia',
     templateUrl: './nova-transferencia.component.html',
-    styleUrls: ['./novas-transferencia.component.scss']
+    styleUrls: ['./nova-transferencia.component.scss']
 })
 
 export class NovaTransferenciaComponent{
